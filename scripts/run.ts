afef35ae1e12076b44c400d8338479e6dd713a08
diefbell <diefenbaker.bell@gmail.com>
2024-12-01 19:49:58 +0000

    Removed running script

--- scripts/run.ts
+++
@@ -1,7 +0,0 @@
-import * as path from "node:path";
-
-const [,, dayNo, taskNo ] = Bun.argv;
-const filePath = path.join(__dirname, "..", "src", dayNo, taskNo, "index.ts");
-
-console.info(`Running code at: "${filePath}\n"`);
-await import(filePath);
