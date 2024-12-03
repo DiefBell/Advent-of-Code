8a3f1321d1ed4fc2a11b7d3a58a343322955919b
diefbell <diefenbaker.bell@gmail.com>
2024-12-03 10:12:02 +0000

    day 3 part 2

--- src/day03/main.py
+++ src/day03/main.py
@@ -1,4 +1,4 @@
-import re as regex
+import re
 from typing import cast
 
 type InputPair = tuple[int, int]
@@ -8,20 +8,38 @@ INPUT_FILE: str = "./input.txt"
 '''
 Parses the file string, returning a list of tuples of values to multiply together.
 '''
-def parse_file(file_input: str) -> list[InputPair]:
-	muls: list[str] = regex.findall("mul\\(\\d+,\\d+\\)", file_input)
+def parse_file(file_input: str, enable_do_dont: bool) -> list[InputPair]:
+	pattern = re.compile("do\\(\\)|don\\'t\\(\\)|mul\\(\\d+,\\d+\\)")
+	tokens: list[str] = pattern.findall(file_input)
+
 	pairs: list[InputPair] = []
 
-	for m in muls:
-		num_strings = cast(tuple[str, str], regex.findall("\\d+", m))
-		pair = cast(InputPair, tuple(int(s) for s in num_strings))
-		pairs.append(pair)
+	do = True
+	for token in tokens:
+		if token == "do()":
+			do = not enable_do_dont or True
+		elif token == "don't()":
+			do = not enable_do_dont or False
+		elif do:
+			num_strings = cast(tuple[str, str], re.findall("\\d+", token))
+			pair = cast(InputPair, tuple(int(s) for s in num_strings))
+			pairs.append(pair)
 	
 	return pairs
 
+def part_one(fileContent: str):
+	pairs = parse_file(fileContent, False)
+	result = sum(a * b for a, b in pairs)
+	print(f"Result for part one: {result}")
+
+def part_two(fileContent: str):
+	pairs = parse_file(fileContent, True)
+	result = sum(a * b for a, b in pairs)
+	print(f"Result for part two: {result}")
+
+
 if __name__ == "__main__":
 	f = open(INPUT_FILE, "r")
-	fileContent = f.read()
-	pairs = parse_file(fileContent)
-	result = sum(a * b for a, b in pairs)
-	print(f"Result: {result}")
+	file_content = f.read()
+	part_one(file_content)
+	part_two(file_content)
