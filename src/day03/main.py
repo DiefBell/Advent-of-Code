559f49d9523cc83386109c3d1d816514dde531d4
diefbell <diefenbaker.bell@gmail.com>
2024-12-03 09:49:08 +0000

    Day 3 part 1

--- src/day03/main.py
+++ src/day03/main.py
@@ -1,1 +1,27 @@
-print("\n\nHello world")
+import re as regex
+from typing import cast
+
+type InputPair = tuple[int, int]
+
+INPUT_FILE: str = "./input.txt"
+
+'''
+Parses the file string, returning a list of tuples of values to multiply together.
+'''
+def parse_file(file_input: str) -> list[InputPair]:
+	muls: list[str] = regex.findall("mul\\(\\d+,\\d+\\)", file_input)
+	pairs: list[InputPair] = []
+
+	for m in muls:
+		num_strings = cast(tuple[str, str], regex.findall("\\d+", m))
+		pair = cast(InputPair, tuple(int(s) for s in num_strings))
+		pairs.append(pair)
+	
+	return pairs
+
+if __name__ == "__main__":
+	f = open(INPUT_FILE, "r")
+	fileContent = f.read()
+	pairs = parse_file(fileContent)
+	result = sum(a * b for a, b in pairs)
+	print(f"Result: {result}")
