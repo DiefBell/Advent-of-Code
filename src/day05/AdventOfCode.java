f2627d092520ef2330577677540d6d75b7d6677e
diefbell <diefenbaker.bell@gmail.com>
2024-12-04 18:41:21 +0000

    Multiple options for reading a file in Java

--- src/day05/AdventOfCode.java
+++ src/day05/AdventOfCode.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,11 +12,20 @@ public class AdventOfCode {
     public static void main(String[] args) {
         String filePath = "input.txt";
 
+		// line-by-line
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
             }
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "An error occurred while reading the file", e);
+        }
+
+		// whole file in single string
+		try {
+            String content = Files.readString(Paths.get(filePath)); // Java 11 and above
+            System.out.println(content);
         } catch (IOException e) {
             LOGGER.log(Level.SEVERE, "An error occurred while reading the file", e);
         }
