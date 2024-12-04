c1cda6957fb4a2f41c9d9405732a9648a5b0b222
diefbell <diefenbaker.bell@gmail.com>
2024-12-04 09:55:32 +0000

    Day 4 part 1

--- src/day04/Program.cs
+++ src/day04/Program.cs
@@ -1,2 +1,125 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+string[] lines = File.ReadAllLines("input.txt");
+int rows = lines.Length;
+
+bool ElementsAreMas(char first, char second, char third)
+{
+	return first == 'M' && second == 'A' && third == 'S';
+}
+
+int count = 0;
+for(int row = 0; row < rows; row++)
+{
+	string line = lines[row];
+	int cols = line.Length;
+
+	for(int col = 0; col < cols; col++)
+	{
+		char letter = line[col];
+		if(letter != 'X')
+		{
+			continue;
+		}
+
+		// diagonally up-left
+		if(col > 2 && row > 2)
+		{
+			char l2 = lines[row-1][col-1];
+			char l3 = lines[row-2][col-2];
+			char l4 = lines[row-3][col-3];
+			if(ElementsAreMas(l2, l3, l4))
+			{
+				count++;
+			}
+		}
+
+		// straight left
+		if(col > 2)
+		{
+			char l2 = lines[row][col-1];
+			char l3 = lines[row][col-2];
+			char l4 = lines[row][col-3];
+			if(ElementsAreMas(l2, l3, l4))
+			{
+				count++;
+			}
+		}
+
+		// diagonally down-left
+		if(col > 2 && row < rows - 3)
+		{
+			char l2 = lines[row+1][col-1];
+			char l3 = lines[row+2][col-2];
+			char l4 = lines[row+3][col-3];
+			if(ElementsAreMas(l2, l3, l4))
+			{
+				count++;
+			}
+		}
+
+		// straight down
+		if(row < rows - 3)
+		{
+			char l2 = lines[row+1][col];
+			char l3 = lines[row+2][col];
+			char l4 = lines[row+3][col];
+			if(ElementsAreMas(l2, l3, l4))
+			{
+				count++;
+			}
+		}
+
+		// diagonally down-right
+		if(row < rows - 3 && col < cols - 3)
+		{
+			char l2 = lines[row+1][col+1];
+			char l3 = lines[row+2][col+2];
+			char l4 = lines[row+3][col+3];
+			if(ElementsAreMas(l2, l3, l4))
+			{
+				count++;
+			}
+		}
+		
+		// straight right
+		if(col < cols - 3)
+		{
+			char l2 = lines[row][col+1];
+			char l3 = lines[row][col+2];
+			char l4 = lines[row][col+3];
+			if(ElementsAreMas(l2, l3, l4))
+			{
+				count++;
+			}
+		}
+		
+		// diagonally up-right
+		if(row > 2 && col < cols - 3)
+		{
+			char l2 = lines[row-1][col+1];
+			char l3 = lines[row-2][col+2];
+			char l4 = lines[row-3][col+3];
+			if(ElementsAreMas(l2, l3, l4))
+			{
+				count++;
+			}
+		}
+		
+		// straight up
+		if(row > 2)
+		{
+			char l2 = lines[row-1][col];
+			char l3 = lines[row-2][col];
+			char l4 = lines[row-3][col];
+			if(ElementsAreMas(l2, l3, l4))
+			{
+				count++;
+			}
+		}
+	}
+}
+
+Console.WriteLine($"Found XMAS {count} times.");
