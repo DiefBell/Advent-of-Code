90805ea060c65722895d5bc213899282ffe28baa
diefbell <diefenbaker.bell@gmail.com>
2024-12-10 09:23:29 +0000

    Refactor main code into a function

--- src/day09/advent_of_code.pl
+++ src/day09/advent_of_code.pl
@@ -9,7 +9,6 @@ use EmptySpace;        # EmptySpace class
 use DiskItem;          # DiskItem class
 
 # File path
-# my $file_path = 'input.sample.txt';
 my $file_path = 'input.txt';
 
 # Open the file
@@ -42,38 +41,46 @@ for my $index (0 .. length($line) - 1) {
     }
 }
 
-my @items = $disk->to_array();
+# Call the part_one function to process the disk and calculate the checksum
+part_one($disk);
 
-my $start_index = 0;
-my $end_index = $#items;  # $#items gives the last index
+# Define the part_one function
+sub part_one {
+    my ($disk) = @_;
 
-while ($start_index < $end_index) {
-    my $start = $items[$start_index];
-	my $end = $items[$end_index];
+    my @items = $disk->to_array();
 
-	if(defined($start)) {
-		$start_index++;
-		next;
-	}
-	if(!defined($end)) {
-		$end_index--;
-		next;
-	}
-	$items[$start_index] = $end;
-	$items[$end_index] = undef;
-	$start_index++;
-	$end_index--;
+    my $start_index = 0;
+    my $end_index = $#items;  # $#items gives the last index
+
+    while ($start_index < $end_index) {
+        my $start = $items[$start_index];
+        my $end = $items[$end_index];
+
+        if(defined($start)) {
+            $start_index++;
+            next;
+        }
+        if(!defined($end)) {
+            $end_index--;
+            next;
+        }
+        $items[$start_index] = $end;
+        $items[$end_index] = undef;
+        $start_index++;
+        $end_index--;
+    }
+
+    my $checksum = 0;  # Initialize the total sum
+
+    # Iterate over the array, stopping when we encounter 'undef'
+    for my $index (0 .. $#items) {
+        # If we encounter 'undef', exit the loop
+        last if !defined($items[$index]);
+
+        # Multiply the value at the index by the index and add it to the total
+        $checksum += $items[$index] * $index;
+    }
+
+    print "Total sum: $checksum\n";
 }
-
-my $checksum = 0;  # Initialize the total sum
-
-# Iterate over the array, stopping when we encounter 'undef'
-for my $index (0 .. $#items) {
-    # If we encounter 'undef', exit the loop
-    last if !defined($items[$index]);
-
-    # Multiply the value at the index by the index and add it to the total
-    $checksum += $items[$index] * $index;
-}
-
-print "Total sum: $checksum\n";
