e8a63d4f42f6ecc1e8e23bcfcca341e0c50dfb43
diefbell <diefenbaker.bell@gmail.com>
2024-12-11 13:34:19 +0000

    Refactor into functions for "parts"

--- src/day11/main.f90
+++ src/day11/main.f90
@@ -3,11 +3,9 @@ program advent_of_code_2024
    integer(kind=8), parameter :: max_length = 256
    character(len=max_length) :: line
    integer(kind=4) :: number
-   integer :: i, num_items, ios, j
+   integer :: i, num_parsed_values, ios
    integer, parameter :: max_items = 100
-   integer(kind=8), dimension(max_items) :: values
-   real :: start_time, end_time, elapsed_time
-   integer(kind=8) :: count, num_stones
+   integer(kind=8), dimension(max_items) :: parsed_values
 
    open(unit=10, file="input.txt", status="old", action="read")
 
@@ -17,43 +15,70 @@ program advent_of_code_2024
       stop
    end if
 
-   num_items = 0
+   num_parsed_values = 0
    do while (len_trim(line) > 0)
       read(line, *, iostat=ios) number
       if (ios /= 0) then
          print *, "Error reading a number!"
          exit
       end if
-      num_items = num_items + 1
-      if (num_items > max_items) then
+      num_parsed_values = num_parsed_values + 1
+      if (num_parsed_values > max_items) then
          print *, "Exceeded max_items!"
          stop
       end if
-      values(num_items) = number
+      parsed_values(num_parsed_values) = number
 
       i = index(line, " ")
       if (i == 0) exit
       line = adjustl(line(i+1:))
    end do
 
-   call cpu_time(start_time)
-
-   count = 0
-   do j = 1, num_items
-      num_stones = blink(values(j), 25)
-      count = count + num_stones
-   end do
-
-   call cpu_time(end_time)
-
-   elapsed_time = end_time - start_time
-
-   write(*,*) CHAR(10) // CHAR(10) // "Final count after processing all values: ", count
-   write(*,*) "Elapsed time for processing: ", elapsed_time * 1000, " milliseconds" // CHAR(10) // CHAR(10)
+   call part_one(parsed_values, num_parsed_values)
+   ! call part_two(parsed_values, num_parsed_values)
 
    close(10)
 
 contains
+
+   subroutine part_one(values, num_items)
+      integer(kind=8), dimension(:), intent(in) :: values
+      integer, intent(in) :: num_items
+
+      write(*,*) CHAR(10) // "Part one:"
+      call count_stones(values, num_items, 25)
+   end subroutine part_one
+
+   subroutine part_two(values, num_items)
+      integer(kind=8), dimension(:), intent(in) :: values
+      integer, intent(in) :: num_items
+
+      write(*,*) CHAR(10) // "Part two:"
+      call count_stones(values, num_items, 75)
+   end subroutine part_two
+
+   subroutine count_stones(values, num_items, depth)
+      integer(kind=8), dimension(:), intent(in) :: values
+      integer, intent(in) :: num_items, depth
+      integer(kind=8) :: count, num_stones
+      real :: start_time, end_time, elapsed_time
+      integer :: j
+
+      call cpu_time(start_time)
+
+      count = 0
+      do j = 1, num_items
+         num_stones = blink(values(j), depth)
+         count = count + num_stones
+      end do
+
+      call cpu_time(end_time)
+
+      elapsed_time = end_time - start_time
+
+      write(*,*) CHAR(9) // "Final count after processing all values: ", count
+      write(*,*) CHAR(9) // "Elapsed time for processing: ", elapsed_time * 1000, " milliseconds" // CHAR(10)
+   end subroutine count_stones
 
    recursive function blink(value, depth) result(res)
       integer(kind=8), intent(in) :: value
@@ -62,7 +87,6 @@ contains
       integer(kind=8) :: left, right
       integer :: num_digits
 
-      ! Recursively processes the value based on its number of digits and depth.
       if (depth <= 0) then
          res = 1_8
       else
@@ -84,7 +108,6 @@ contains
       integer(kind=8), intent(in) :: value
       integer :: num_digits
 
-      ! Returns the number of digits in a given value.
       if (value == 0) then
          num_digits = 1
       else
@@ -98,7 +121,6 @@ contains
       integer(kind=8), intent(out) :: res_left, res_right
       integer(kind=8) :: divisor
 
-      ! Splits the value into two parts based on its number of digits.
       if (mod(num_digits, 2) /= 0) then
          print *, "Error: num_digits must be even!"
          stop
