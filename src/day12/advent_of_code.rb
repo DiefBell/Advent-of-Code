de21d76850df06de3c5cb18e2a640a563ccb8e9b
diefbell <diefenbaker.bell@gmail.com>
2024-12-12 17:08:34 +0000

    Getting sides fine, works on sample, not on real data

--- src/day12/advent_of_code.rb
+++ src/day12/advent_of_code.rb
@@ -1,4 +1,5 @@
 require 'set'
+require 'text-table'
 
 class Coord
   attr_accessor :x, :y
@@ -92,18 +93,33 @@ class Garden
       end
     end
 
-    # Print to_s of each region in @regions
-    @regions.each do |region|
-      puts region.to_s
-    end
+    # Now create the table data for all regions
+    table_data = @regions.map(&:get_table_data)
+
+    # Set up the table with headers and data
+    table = Text::Table.new
+    table.head = [
+      'Letter',
+      'Plots',
+      'Fences',
+      'Sides',
+      'Price',
+      'Discounted'
+    ]
+    table.rows = table_data
+
+    # Display the table
+    puts table
   end
 
   def to_s
-    total_cost = @regions.sum { |region| region.get_cost }
-    "Width: #{@width}, " +
+    total_price = @regions.sum { |region| region.get_price }
+    total_discounted_price = @regions.sum { |region| region.get_discounted_price }
+    "\nWidth: #{@width}, " +
     "Height: #{@height}. " +
     "Unique regions: #{@regions.length}. " +
-    "Total cost: #{total_cost}"
+    "Total cost: #{total_price}, " +
+    "Discounted price: #{total_discounted_price}"
   end
 end
 
@@ -159,21 +175,134 @@ class Region
   def get_area
     @plots.length
   end
+end
 
-  def get_cost
+class Region
+  attr_accessor :fences, :plots
+
+  def initialize(letter, plots, fences)
+    @letter = letter
+    @plots = Set.new(plots)  # Use Set to ensure uniqueness
+    @fences = Set.new(fences)  # Use Set to ensure uniqueness
+  end
+
+  # Sort vertical fences (x coordinate off by 0.5)
+  def sort_vertical_fences
+    # Select only vertical fences (where x is off by 0.5)
+    vertical_fences = @fences.select { |f| f.x != f.x.to_i }
+
+    # Group by x coordinate and then sort by y coordinate within each group
+    vertical_groups = vertical_fences.group_by { |f| f.x }
+
+    # Sort the groups by x (outer sort), and within each group, sort by y (inner sort)
+    vertical_groups.each do |x, fences|
+      fences.sort_by! { |f| f.y }  # Sort fences within the group by y
+    end
+
+    # Now sort the groups by x coordinate
+    sorted_vertical_fences = vertical_groups.sort_by { |x, _| x }.flat_map { |_, fences| fences }
+
+    # Return the sorted list of fences
+    sorted_vertical_fences
+  end
+
+  # Sort horizontal fences (y coordinate off by 0.5)
+  def sort_horizontal_fences
+    # Select only horizontal fences (where y is off by 0.5)
+    horizontal_fences = @fences.select { |f| f.y != f.y.to_i }
+
+    # Group by y coordinate and then sort by x coordinate within each group
+    horizontal_groups = horizontal_fences.group_by { |f| f.y }
+
+    # Sort the groups by y (outer sort), and within each group, sort by x (inner sort)
+    horizontal_groups.each do |y, fences|
+      fences.sort_by! { |f| f.x }  # Sort fences within the group by x
+    end
+
+    # Now sort the groups by y coordinate
+    sorted_horizontal_fences = horizontal_groups.sort_by { |y, _| y }.flat_map { |_, fences| fences }
+
+    # Return the sorted list of fences
+    sorted_horizontal_fences
+  end
+
+  # Calculates the number of sides for this region
+  def get_sides
+    # Sort both vertical and horizontal fences
+    vertical_fences = sort_vertical_fences
+    horizontal_fences = sort_horizontal_fences
+
+    vertical_sides = 1
+    horizontal_sides = 1
+
+    # Count vertical sides
+    (1...vertical_fences.length).each do |i|
+      fence = vertical_fences[i]
+      prev = vertical_fences[i-1]
+
+      if fence.x != prev.x
+        # Fences aren't even in a line
+        vertical_sides += 1
+      elsif fence.y - prev.y > 1
+        # They're not joined
+        vertical_sides += 1
+      end
+    end
+
+    # Count horizontal sides
+    (1...horizontal_fences.length).each do |i|
+      fence = horizontal_fences[i]
+      prev = horizontal_fences[i-1]
+
+      if fence.y != prev.y
+        # Fences aren't even in a line
+        horizontal_sides += 1
+      elsif fence.x - prev.x > 1
+        # They're not joined
+        horizontal_sides += 1
+      end
+    end
+
+    # The total number of sides is the sum of vertical and horizontal sides
+    total_sides = vertical_sides + horizontal_sides
+    total_sides
+  end  
+  
+  def get_price
     get_area * @fences.length
   end
 
+  def get_discounted_price
+    get_area * get_sides
+  end
+
+  # Get table data for this region
+  def get_table_data
+    letter = @letter
+    plot_number = @plots.length
+    fences_number = @fences.length
+    sides_number = get_sides
+    usual_price = get_price
+    discounted_price = get_discounted_price
+
+    [letter, plot_number, fences_number, sides_number, usual_price, discounted_price]
+  end
+
   def to_s
-    cost = @get_cost
     "[Region] Letter '#{@letter}', " +
     "Plots: #{@plots.length}, " +
     "Fences: #{@fences.length}, " +
-    "Cost: #{get_cost}"
+    "Sides: #{get_sides}, " +
+    "Usual price: #{get_price}, " + 
+    "Discounted price: #{get_discounted_price}"
   end
 end
 
 puts "\n"
 # garden = Garden.new("input.sample.txt")
+# garden = Garden.new("input.test.txt")
 garden = Garden.new("input.txt")
 puts garden.to_s
+
+# 608664 is too low
+# 814778 ain't right
