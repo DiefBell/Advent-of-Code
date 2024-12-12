8f3861f011f8c0104f7d938c304ac187ce51c8fd
diefbell <diefenbaker.bell@gmail.com>
2024-12-12 09:59:26 +0000

    Creating some kinda plot object

--- src/day12/advent_of_code.rb
+++ src/day12/advent_of_code.rb
@@ -1,18 +1,42 @@
-
 class Garden
 	def initialize(file_path)
 		file_content = File.read(file_path)
 		lines = file_content.split("\n")
-
 		@height = lines.length
 		@width = lines.first.length
-		@plots = lines.join
+
+		@plots = lines.map do |row|
+			row.chars.map { |char| Plot.new(char) }
+		end
 	end
 
-	def to_string
+	def to_s
 		"Width: #{@width}, Height: #{@width}."
 	end
 end
 
+class Plot
+	def initialize(letter)
+		@letter = letter;
+		@region = nil;
+	end
+end
+
+class Region
+	def initialize(plots, fences)
+		@plots = plots
+		@area = plots.length
+		@fences = fences
+	end
+
+	def self.merge(a, b)
+		return Region.new(a + b)
+	end
+
+	def get_cost()
+		return @area * @fences
+	end
+end
+
 garden = Garden.new("input.sample.txt")
-puts garden.to_string
+puts garden.to_s
