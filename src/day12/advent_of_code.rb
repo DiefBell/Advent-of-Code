f53df20176beff0e2a27a873c989f56056e89009
diefbell <diefenbaker.bell@gmail.com>
2024-12-12 09:42:58 +0000

    Parse input in a class

--- src/day12/advent_of_code.rb
+++ src/day12/advent_of_code.rb
@@ -1,2 +1,18 @@
-file_content = File.read('input.txt')
-puts file_content
+
+class Garden
+	def initialize(file_path)
+		file_content = File.read(file_path)
+		lines = file_content.split("\n")
+
+		@height = lines.length
+		@width = lines.first.length
+		@plots = lines.join
+	end
+
+	def to_string
+		"Width: #{@width}, Height: #{@width}."
+	end
+end
+
+garden = Garden.new("input.sample.txt")
+puts garden.to_string
