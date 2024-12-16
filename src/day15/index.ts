f9d607a8055fbc695588124f3051a600eed9dbaa
diefbell <diefenbaker.bell@gmail.com>
2024-12-16 19:22:39 +0000

    Need to revisit: Need to factor in double-wide boxes pushing other boxes

--- src/day15/index.ts
+++ src/day15/index.ts
@@ -1,7 +1,7 @@
 import * as path from "path";
 import * as fs from "fs";
 
-const SAMPLE = false;
+const SAMPLE = true;
 
 const inputPath = path.join(__dirname, SAMPLE ? "input.sample.txt" : "input.txt");
 const inputText = fs.readFileSync(inputPath, "utf-8");
@@ -40,6 +40,8 @@ const BOX = "O";
 
 const partOne = () =>
 {
+	console.log("\n=== Part One ===");
+	console.time("Part One");
 	const roomLines = roomText.split("\n");
 	const roomWidth = roomLines[0].length;
 	const room = roomLines.join("").split("");
@@ -51,11 +53,14 @@ const partOne = () =>
 			.map((char, index) => index)
 			.filter((index) => room[index] === WALL)
 	);
+	console.log("Walls", wallIndices.size);
+
 	const boxIndices = new Set(
 		room
 			.map((char, index) => index)
 			.filter((index) => room[index] === BOX)
 	);
+	console.log("Boxes", boxIndices.size);
 
 	let robotIndex = room.indexOf(ROBOT);
 
@@ -93,17 +98,167 @@ const partOne = () =>
 	});
 
 	const gpsTotal = boxesGps.reduce((prev, curr) => prev + curr, 0);
-	console.log(`[part one] GPS total: ${gpsTotal}`);
+	console.log(`GPS total: ${gpsTotal}`);
+	console.timeEnd("Part One");
 };
 
 const partTwo = () =>
 {
+	console.log("\n=== Part Two ===");
+	console.time("Part Two");
 
+	type Box = {
+		leftIndex: number,
+		rightIndex: number,
+	};
+
+	const WIDE_BOX_LEFT = "[";
+	const WIDE_BOX_RIGHT = "]";
+
+	let doubleRoomStr = "";
+	for (const char of Array.from(roomText))
+	{
+		switch (char)
+		{
+			case "\n":
+				doubleRoomStr += "\n";
+				break;
+			case ".":
+				doubleRoomStr += "..";
+				break;
+			case ROBOT:
+				doubleRoomStr += ROBOT + ".";
+				break;
+			case WALL:
+				doubleRoomStr += WALL + WALL;
+				break;
+			case BOX:
+				doubleRoomStr += WIDE_BOX_LEFT + WIDE_BOX_RIGHT;
+				break;
+			default:
+				throw new Error(`Encountered unknown room input value: "${char}"`);
+		}
+	}
+	console.log(doubleRoomStr);
+
+	const roomWidth = doubleRoomStr[0].length;
+	const doubleRoom = doubleRoomStr.split("\n").join("").split("");
+
+	const moves = movesString.split("").map((char) => charToMoveString(char, roomWidth));
+
+	const wallIndices = new Set(
+		doubleRoom
+			.map((char, index) => index)
+			.filter((index) => doubleRoom[index] === WALL)
+	);
+	console.log("Walls", wallIndices.size);
+
+	const boxIndices = new Set(
+		doubleRoom
+			.map((char, index) => index)
+			.filter(
+				(index) => doubleRoom[index] === WIDE_BOX_LEFT || doubleRoom[index] === WIDE_BOX_RIGHT
+			)
+	);
+	console.log("Boxes", boxIndices.size / 2);
+
+	const boxes: Record<number, Box> = {};
+	boxIndices.forEach(
+		(idx) =>
+		{
+			// In initial setup, all boxes' left side will be on even position
+			if(idx % 2 !== 0) return;
+			const box: Box = {
+				leftIndex: idx,
+				rightIndex: idx + 1,
+			};
+			boxes[idx] = box;
+			boxes[idx + 1] = box;
+		}
+	);
+
+	let robotIndex = doubleRoom.indexOf(ROBOT);
+
+	for (const move of moves)
+	{
+		const horizontal = Math.abs(move) === 1; // left or right movement
+		const newBoxes: Box[] = [];
+
+		let i = robotIndex + move;
+		let box = boxes[i];
+		while (box !== undefined)
+		{
+			delete boxes[box.leftIndex];
+			delete boxes[box.rightIndex];
+			newBoxes.push({
+				leftIndex: box.leftIndex + move,
+				rightIndex: box.rightIndex + move,
+			});
+
+			i += horizontal ? move * 2 : move;
+			box = boxes[i];
+		}
+
+		// means next non-box position is a wall
+		if(wallIndices.has(i))
+		{
+			continue;
+		}
+
+		// make sure we work backwards so we don't have overlapping boxes that get deleted
+		for (const newBox of newBoxes.reverse())
+		{
+			boxes[newBox.leftIndex] = newBox;
+			boxes[newBox.rightIndex] = newBox;
+		}
+		robotIndex += move;
+	}
+
+	let str = "";
+	for (let i = 0; i < doubleRoom.length; i++)
+	{
+		if(i === robotIndex)
+		{
+			str += "@";
+		}
+		else if(wallIndices.has(i))
+		{
+			str += "#";
+		}
+		else if(boxes[i])
+		{
+			str += "[]";
+			i++;
+		}
+		else
+		{
+			str += ".";
+		}
+
+		if((i + 1) % roomWidth === 0)
+		{
+			str += "\n";
+		}
+	}
+	console.log(str);
+
+	const boxesGps = Object.values(boxes).map((box: Box) =>
+	{
+		const x = box.leftIndex % roomWidth;
+		const y = Math.floor(box.leftIndex / roomWidth);
+
+		return 100 * y + x;
+	});
+
+	const gpsTotal = boxesGps.reduce((prev, curr) => prev + curr, 0);
+	console.log(`GPS total: ${gpsTotal}`);
+	console.timeEnd("Part Two");
 };
 
 // Just exporting to make this a module
 export const day15 = () =>
 {
 	partOne(); // 1463512
+	partTwo();
 };
 day15();
